5d75ec3976bf4f14e7780cd76c1827c7927b88c0
lscyane <lscyane@gmail.com>
2025-08-31 22:32:12 +0900

    XAMLの名前空間をURLで定義できるように対応

---
+++ lscyane.Wpf/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: System.Windows.ThemeInfo(
+    System.Windows.ResourceDictionaryLocation.None,
+    System.Windows.ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition(xmlNamespace: @"http://cyane.info/wpf", clrNamespace: "lscyane.Wpf")]
+[assembly: XmlnsDefinition(xmlNamespace: @"http://cyane.info/wpf", clrNamespace: "lscyane.Wpf.AttachedProperty")]
+[assembly: XmlnsDefinition(xmlNamespace: @"http://cyane.info/wpf", clrNamespace: "lscyane.Wpf.Control")]
+[assembly: XmlnsDefinition(xmlNamespace: @"http://cyane.info/wpf", clrNamespace: "lscyane.Wpf.Converter")]
+[assembly: XmlnsDefinition(xmlNamespace: @"http://cyane.info/wpf", clrNamespace: "lscyane.Wpf.Extention")]
+
