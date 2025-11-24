2c91bc40b580c93ce96ddfe1c6a422c01713f6bb
lscyane <lscyane@gmail.com>
2025-11-24 14:05:15 +0900

    バインド値チェック用テストコンバータ追加

---
+++ lscyane.Wpf/Converter/Test.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace lscyane.Wpf.Converter
+{
+    /// <summary>
+    /// test
+    /// </summary>
+    public class Test : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+
+
+        /// <summary> 値コンバーターの実体 </summary>
+        public static Test Converter = new Test();
+    }
+}
